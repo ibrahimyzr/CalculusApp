d6aa1af2db3937a4556804d449d803ce4aa6c78f
ibrahimyzr <yuzeribrahim@gmail.com>
2025-02-04 15:21:28 +0300

    04.02.2025

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import db from "../firebase/firebase";
+import db from "../firebase/firebase.config";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
 
 function Contact() {
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const db = getFirestore(app);
 
-export default auth;
+export default db;
